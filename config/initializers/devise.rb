c5aece2e4bd5cdfad1ace5dd1c1e18eecb2704c7
sierraskyewallace <sskwallace@gmail.com>
2021-05-27 13:28:45 -0700

    changed config devise line to try to fix routing error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,9 +274,10 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   client_id = Rails.application.secrets[:google_client_id]
   client_secret = Rails.application.secrets[:google_secret]
-  config.omniauth :google_oauth2, client_id, client_secret, {
-    scope: "contacts.readonly,userinfo.email"
-  }
+  #config.omniauth :google_oauth2, client_id, client_secret, {
+  #  scope: "contacts.readonly,userinfo.email"
+ # }
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], access_type: "online"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root to: "sessions#home"
   #devise_for :users
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+  
   #get '/auth/:provider/callback' => 'sessions#omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

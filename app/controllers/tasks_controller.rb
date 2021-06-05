1d2f6fd250cd8123e74abb8925b334876ad1e2b7
sierraskyewallace <sskwallace@gmail.com>
2021-06-05 13:53:47 -0700

    added creud actions and private methods to task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,32 @@
 class TasksController < ApplicationController
-end
+    before_action :set_task
+    def index
+        @task = Task.all
+      end
+    
+      def create
+        @task = @list.tasks.build(task_params)
+        @task.save
+        redirect_to @list
+      end
+      
+      def edit
+        @task.update(task_params)
+    
+        redirect_to list_path(@task.list)
+      end
+    
+      def destroy
+        @task.destroy
+        redirect_to list_path(@task.list)
+      end
+    
+      private
+      
+      def task_params
+        params.require(:task).permit(:name, :status)
+      end
+      def set_task 
+        @task = Task.find_by(params[:id])
+      end
+    end

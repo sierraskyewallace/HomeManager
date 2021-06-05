cbf7e40212984d8f6b2fa384a1603f9de9727ebc
sierraskyewallace <sskwallace@gmail.com>
2021-06-05 13:52:18 -0700

    added crud actions and provte methods for params and set list to lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,40 @@
 class ListsController < ApplicationController
+    before_action :set_list
+    def index
+        @list = List.new
+        @lists = List.all
+      end
+    
+      def show
+        @task = Task.new
+      end
+    
+      def new 
+        @list = List.new
+        @list.tasks.build
+      end
+    
+      def create
+        @list = List.new(list_params)
+        if @list.save
+          redirect_to @list
+        else
+          render :new
+        end
+      end
+    
+      def destroy
+        @task = Task.find_by(params[:id])
+        @task.destroy
+        redirect_to @list
+      end
+    
+      private
+    
+      def list_params
+        params.require(:list).permit(:name, tasks_attributes: [:name, :completed])
+      end
+      def set_list 
+        @list = List.find_by(params[:id])
+      end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,9 @@
 class List < ApplicationRecord
+  has_many :tasks
+  has_many :user_lists
+  has_many :users, through: :user_lists
+
+  validates_presence_of :name, uniqueness: true
+
+  accepts_nested_attributes_for :tasks
 end

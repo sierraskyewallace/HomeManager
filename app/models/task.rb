d94685d9fa26497703dc279a8b8a382390435ed4
sierraskyewallace <sskwallace@gmail.com>
2021-06-09 14:19:22 -0700

    added team resource to possibly create teams of users and assign taks to fellow users

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
     belongs_to :list
-
+    has_many :users, through: :lists
     validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :teams
   root to: "sessions#home"
 
   resources :lists do

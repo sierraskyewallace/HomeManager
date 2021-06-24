00b277f4ea5730b13059afd62ec0955e250f6e50
sierraskyewallace <sskwallace@gmail.com>
2021-06-24 16:59:44 -0700

    trying to get users assigned to a group upon invite

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,22 +1,6 @@
 class Users::InvitationsController < Devise::InvitationsController
 
-    def edit
-        sign_out send("current_#{resource_name}") if send("#{resource_name}_signed_in?")
-        set_minimum_password_length
-        resource.invitation_token = params[:invitation_token]
-        redirect_to "http://localhost:8080/users/invitation/accept?invitation_token=#{params[:invitation_token]}"
-      end
-    
-      def update
-        super do |resource|
-          if resource.errors.empty?
-            render json: { status: "Invitation Accepted!" }, status: 200 and return
-          else
-            render json: resource.errors, status: 401 and return
-          end
-        end
-      end
-      
+
     def new
         @group = Group.find_by_id(params[:group_id])
         @user = @group.users.new
@@ -28,8 +12,6 @@ class Users::InvitationsController < Devise::InvitationsController
         @user = @group.users.new(group_user_params)
         @user.groups << @group
         @user.invite!
-        
-        
       end
 
     

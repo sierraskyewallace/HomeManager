d67b11208d50746ea9485f3227800c3b7e311d4f
sierraskyewallace <sskwallace@gmail.com>
2021-07-06 09:46:42 -0700

    updated group and group member migrations

--- db/migrate/20210706163708_create_groups.rb
+++ db/migrate/20210706163708_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[6.1]
   def change
     create_table :groups do |t|
-
+      t.integer :group_member_id
       t.timestamps
     end
   end
--- db/migrate/20210706163720_create_group_members.rb
+++ db/migrate/20210706163720_create_group_members.rb
@@ -1,7 +1,8 @@
 class CreateGroupMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :group_members do |t|
-
+      t.integer :user_id 
+      t.integer :group_id
       t.timestamps
     end
   end

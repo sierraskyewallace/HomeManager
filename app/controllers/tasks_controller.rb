d12dbda769f5af0521b2353613780ae1a030fd4c
sierraskyewallace <sskwallace@gmail.com>
2021-07-06 12:37:24 -0700

    got tasks controller crud actions stubbed out

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,43 +2,52 @@ class TasksController < ApplicationController
   before_action :set_task
 
     def index
+      @tasks = Task.all
+    end
 
-      end
-    
-      def show
+    def show 
+    end
 
-      end
+    def new 
+      @task = Task.new 
+    end
     
-      def new
-
-      end
-    
-      def create
-       
+    def create
+       @task = Task.new(task_params)
+          if @task.save 
+            redirect_to @task 
+          else 
+            render :new 
+          end
         end
 
-        def edit
+    def edit
+    end
 
-        end
-
-        def update
-        end
+   def update
+    if @task.update(task_params)
+      redirect_to @task 
+    else 
+      render :edit 
+    end
+   end
 
     
 
-      def destroy
-
-        end
+   def destroy
+    @task.destroy 
+    redirect_to tasks_path
+   end
     
       private
  
-        def set_task
-          @task = Task.find_by_id(params[:id])
-        end
+   def set_task
+     @task = Task.find_by_id(params[:id])
+   end
 
 
-        def task_params
-          params.require(:task).permit(:name)
-        end
+   def task_params
+      params.require(:task).permit(:name)
+      end
     end
     

fd8e1fa2f30463a50375289a214c942ca8a62b33
sierraskyewallace <sskwallace@gmail.com>
2021-07-06 12:47:18 -0700

    added current routes to routes.rb as comments for quick ref

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,28 @@ Rails.application.routes.draw do
   end
 end
 
-    
+#group_group_members GET      /groups/:group_id/group_members(.:format)     group_members#index
+#                    POST     /groups/:group_id/group_members(.:format)       group_members#create
+#new_group_group_member GET      /groups/:group_id/group_members/new(.:format)      group_members#new
+#edit_group_group_member GET      /groups/:group_id/group_members/:id/edit(.:format)       group_members#edit
+#group_group_member GET      /groups/:group_id/group_members/:id(.:format)   group_members#show
+
+
+#                  PATCH    /groups/:group_id/group_members/:id(.:format)      group_members#update
+#                    PUT      /groups/:group_id/group_members/:id(.:format)      group_members#update
+#                    DELETE   /groups/:group_id/group_members/:id(.:format)  group_members#destroy
+#                    
+# groups             GET      /groups(.:format)         groups#index
+#                    POST     /groups(.:format)           groups#create
+# new_group          GET      /groups/new(.:format)             groups#new
+#edit_group          GET      /groups/:id/edit(.:format)            groups#edit
+#  group             GET      /groups/:id(.:format)                 groups#show
+#                    PATCH    /groups/:id(.:format)              groups#update
+#                    PUT      /groups/:id(.:format)           groups#update
+#                    DELETE   /groups/:id(.:format)            groups#destroy                  
+                    
+
+
 
  
  

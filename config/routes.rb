4bd46281559a4382e39be9bd29b61090aa867eee
sierraskyewallace <sskwallace@gmail.com>
2021-07-08 11:25:29 -0700

    devise invitable finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "sessions#home"
 
-  devise_for :users, :controllers => {omniauth_callbacks: "users/omniauth_callbacks" } 
+  devise_for :users, :controllers => {omniauth_callbacks: "users/omniauth_callbacks", invitations: 'users/invitations' } 
 
   resources :lists do
     resources :tasks
